10ebfd024707b8a29de9d7008da3debc3341e402
DLizanaDev <Helgait@usal.es>
2025-08-08 19:17:02 +0200

    Added new controller and new method using @PathVariable

--- cursoSpringBoot/src/main/java/cursoSpringBoot/controllers/HolaMundoRestController.java
+++ cursoSpringBoot/src/main/java/cursoSpringBoot/controllers/HolaMundoRestController.java
@@ -8,6 +8,7 @@ public class HolaMundoRestController {
 
     @GetMapping({"/hola", "/hm", "/hello"})
     public String holaMundo(){
+        System.out.println("Solicitud ejecutada.");
         return "Hola, mundo.";
     }
 
---
+++ cursoSpringBoot/src/main/java/cursoSpringBoot/controllers/SaludoRestController.java
@@ -0,0 +1,16 @@
+package cursoSpringBoot.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SaludoRestController {
+
+    @GetMapping("/saludo/{name}")
+    public String saludo(@PathVariable String name){
+        return "Hola, " + name;
+    }
+
+}
