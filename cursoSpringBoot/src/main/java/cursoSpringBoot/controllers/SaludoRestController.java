719783d3cdc746c396d55de0314442246373143e
DLizanaDev <Helgait@usal.es>
2025-08-08 19:21:29 +0200

    Added different possibilities to @GetMapping

--- cursoSpringBoot/src/main/java/cursoSpringBoot/controllers/SaludoRestController.java
+++ cursoSpringBoot/src/main/java/cursoSpringBoot/controllers/SaludoRestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SaludoRestController {
 
-    @GetMapping("/saludo/{name}")
+    @GetMapping({"/saludo/{name}", "/hola/{name}"})
     public String saludo(@PathVariable String name){
         return "Hola, " + name;
     }
