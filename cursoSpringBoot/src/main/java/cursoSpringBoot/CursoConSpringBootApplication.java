f56fba4f9ae485643b5b59143940e75064954d84
DLizanaDev <Helgait@usal.es>
2025-08-08 18:44:07 +0200

    Added controllers

--- cursoSpringBoot/src/main/java/cursoSpringBoot/CursoConSpringBootApplication.java
+++ cursoSpringBoot/src/main/java/cursoSpringBoot/CursoConSpringBootApplication.java
@@ -3,11 +3,24 @@ package cursoSpringBoot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Se usa @SpringBootApplication para marcar la clase principal de la aplicacion en Spring Boot
+// @RestController se usa para marcar la clase de Java como un controlador REST
+// combina @Controller y @ResponseBody para manejar solicitudes HTTP y devolver datos en json o xml. Permite crear controlladores.
+// @GetMapping se usa para mapear las solicitudes HTTP GET, asigna la URL especificada a un metodo
+
+
 @SpringBootApplication
 public class CursoConSpringBootApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CursoConSpringBootApplication.class, args);
+
+
+
+
+
+
 	}
 
 }
