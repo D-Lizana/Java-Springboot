91ea8867464637e590312ce51e1fdfd66d645d6b
DLizanaDev <Helgait@usal.es>
2025-08-09 19:22:41 +0200

    Started practise project app to manage students using Spring Boot

---
+++ AlumnosApp/src/main/java/AlumnosApp/AlumnosAppApplication.java
@@ -0,0 +1,13 @@
+package AlumnosApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumnosAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlumnosAppApplication.class, args);
+	}
+
+}
---
+++ AlumnosApp/src/test/java/AlumnosApp/AlumnosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package AlumnosApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumnosAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
