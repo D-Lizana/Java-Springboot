b64c74926e220429baae6c97d2b397228594671f
DLizanaDev <Helgait@usal.es>
2025-08-10 15:32:47 +0200

    Added class students list and @GetMapping

--- AlumnosApp/src/main/java/AlumnosApp/controllers/AlumnosRestControllers.java
+++ AlumnosApp/src/main/java/AlumnosApp/controllers/AlumnosRestControllers.java
@@ -1,7 +1,34 @@
 package AlumnosApp.controllers;
 
+import AlumnosApp.domain.Alumno;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
 @RestController
+
+@RequestMapping("/alumnos")
 public class AlumnosRestControllers {
+
+    private List<Alumno> alumnos = new ArrayList<>(Arrays.asList(
+            new Alumno(1,"Diego", "@myuax.es",32,"DAM")
+    ));
+
+
+    @GetMapping
+    public List<Alumno> getAlumnos(){
+        return alumnos;
+    }
+
+
+
+
+
+
+
 }
